6cb9d8e463b0480ea8a6f5dd94c871966ecdb444
root <ffo16001@student.mdh.se>
2017-10-13 08:22:45 +0000

    Refactor and IMU update

--- mission_control/src/mission_control.cpp
+++ mission_control/src/mission_control.cpp
@@ -1,27 +1,4 @@
-#include "ros/ros.h"
-#include "/root/catkin_ws/devel/include/mission_control/motion.h"
-#include "nav_msgs/Odometry.h"
-
-#define PI 3.14159265
-mission_control::motion message;
-float desiered_height = 2.0;
-float tolerance = desiered_height * 0.5;
-float angleTolerance = 1.0;
-float positionTolerance = 0.15;
-char checkpoint = 'B';
-
-struct position{
-  float x;
-  float y;
-  float z;
-  float yaw;
-  float roll;
-  float pitch;
-};
-
-position pos; //Position of the Naiad.
-position goal; //Current goal for the Naiad to reach.
-
+#include "mission_control.h"
 
 //Set the goal for the Naiad.
 void SetGoal(float x, float y, float z, float yaw, float roll, float pitch)
